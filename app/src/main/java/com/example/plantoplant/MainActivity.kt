8e0d370d6b28cc3bdf550482a3fef05506973311
﻿คʕ•ﻌ•ʔค <joey300@naver.com>
2023-03-19 20:19:11 +0900

    Login + mainFragment
    - 로그인.xml
    - 메인화면xml
      > 프로필
      > 캘린더
      > 오늘 할 일
      > 식물도감
    
    만들어놨음
    
    ※ 아직 로그인 기능이 없어서 실행이 안될 수 도 있음.

--- app/src/main/java/com/example/plantoplant/MainActivity.kt
+++ app/src/main/java/com/example/plantoplant/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.plantoplant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.plantoplant.navigation.CalenderFragment
+import com.example.plantoplant.navigation.PlantEncyclopediaFragment
+import com.example.plantoplant.navigation.ProfileFragment
+import com.example.plantoplant.navigation.TodayFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        when(p0.itemId) {
+            R.id.action_profile -> {
+                var profileFragment = ProfileFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, profileFragment).commit()
+                return true
+            }
+            R.id.action_calender -> {
+                var calenderFragment = CalenderFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, calenderFragment).commit()
+                return true
+            }
+            R.id.action_today -> {
+                var todayFragment = TodayFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, todayFragment).commit()
+                return true
+            }
+            R.id.action_plantEncyclopedia -> {
+                var plantEncyclopediaFragment = PlantEncyclopediaFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, plantEncyclopediaFragment).commit()
+                return true
+            }
+        }
+        return false
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 왜 안뜨지? id bottom_navigation있는데...
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
     }
 }
---
+++ app/src/main/java/com/example/plantoplant/navigation/PlantEncyclopediaFragment.kt
@@ -0,0 +1,15 @@
+package com.example.plantoplant.navigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.plantoplant.R
+
+class PlantEncyclopediaFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view = LayoutInflater.from(activity).inflate(R.layout.fragment_plant_encyclopedia, container, false)
+        return view
+    }
+}
