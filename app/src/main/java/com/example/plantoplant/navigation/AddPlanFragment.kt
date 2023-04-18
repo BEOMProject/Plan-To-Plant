10bb7fa80a87c76fb9d039817d27e6160c0fe671
연재다 <revnque@gmail.com>
2023-04-18 17:54:17 +0900

    addplan 날짜 선택 구현
    addplan fragment에서 날짜 선택 구현 및 아이디 변경

--- app/src/main/java/com/example/plantoplant/navigation/AddPlanFragment.kt
+++ app/src/main/java/com/example/plantoplant/navigation/AddPlanFragment.kt
@@ -1,15 +1,53 @@
 package com.example.plantoplant.navigation
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.plantoplant.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddPlanFragment : Fragment() {
+
+    private lateinit var tvDatePicker: TextView
+    private lateinit var btnDatePicker: Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_addplan, container, false)
+
+        tvDatePicker = view.findViewById(R.id.adddatetextView)
+        btnDatePicker = view.findViewById(R.id.SelectDate)
+
+        val mycalendar = Calendar.getInstance()
+
+        val datePicker = DatePickerDialog.OnDateSetListener { view, year, month, dayofMonth ->
+            mycalendar.set(Calendar.YEAR, year)
+            mycalendar.set(Calendar.MONTH, month)
+            mycalendar.set(Calendar.DAY_OF_MONTH, dayofMonth)
+            updateLabel(mycalendar)
+        }
+
+        btnDatePicker.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                datePicker,
+                mycalendar.get(Calendar.YEAR),
+                mycalendar.get(Calendar.MONTH),
+                mycalendar.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
+
         return view
+    }
+
+    private fun updateLabel(myCalendar : Calendar){
+        val myformat = "yyyy-MM-dd"
+        val sdf = SimpleDateFormat(myformat,Locale.KOREA)
+        tvDatePicker.setText(sdf.format(myCalendar.time))
     }
 }
