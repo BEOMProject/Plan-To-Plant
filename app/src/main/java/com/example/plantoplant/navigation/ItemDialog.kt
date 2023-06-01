23cd346554ef6e8a0e355a06455d6683257a2edc
GiCheols <ngc4665@naver.com>
2023-06-02 01:41:58 +0900

    할 일을 띄울 때 데이터를 Sorting 합니다.

--- app/src/main/java/com/example/plantoplant/navigation/ItemDialog.kt
+++ app/src/main/java/com/example/plantoplant/navigation/ItemDialog.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.activityViewModels
 import com.example.plantoplant.navigation.ToDoViewModel
