e0e26efc022ed070871297d8f12a1a6b9fc4a6a2
Better Jeong <83108398+BetterJeong@users.noreply.github.com>
2023-06-03 04:02:33 +0900

    레이아웃 수정

--- app/src/main/java/com/example/plantoplant/navigation/CalendarAdapter.kt
+++ app/src/main/java/com/example/plantoplant/navigation/CalendarAdapter.kt
@@ -16,48 +16,46 @@ data class ToDoMonths(var id: Int, var date: LocalDate, var toDo: String, var to
 class CalenderAdapter(private val dayList: ArrayList<String>,
                       private val toDoList: ArrayList<ToDoMonths>,
                       private val onItemListener: Context):
-    RecyclerView.Adapter<CalenderAdapter.ItemViewHolder>() {
+    RecyclerView.Adapter<CalenderAdapter.ViewHolder>() {
 
-//    private lateinit var todayTodoList: ArrayList<String>
-
-    class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val dayText: TextView = itemView.findViewById(R.id.dayText)
         val calendarTodoRecyclerView: RecyclerView = itemView.findViewById(R.id.calendarTodoRecyclerView)
     }
 
     //화면 설정
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.calendar_item,parent,false)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.calendar_item, viewGroup,false)
 
-        return ItemViewHolder(view)
+        return ViewHolder(view)
     }
 
     //데이터 설정
-    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         // 날짜 설정
         var day = dayList[position]
-        holder.dayText.text = day
+        viewHolder.dayText.text = day
 
         // 날짜 텍스트 색상 지정(토,일)
         if ((position + 1) % 7 == 0) {
-            holder.dayText.setTextColor(Color.BLUE)
+            viewHolder.dayText.setTextColor(Color.BLUE)
         } else if (position % 7 == 0) {
-            holder.dayText.setTextColor(Color.RED)
+            viewHolder.dayText.setTextColor(Color.RED)
         } else {
-            holder.dayText.setTextColor(Color.BLACK)
+            viewHolder.dayText.setTextColor(Color.BLACK)
         }
 
         // 할 일 목록 설정
         val todayTodoList = ArrayList<String>()
-
         for (todo in toDoList) {
             if (todo.date.dayOfMonth.toString() == day) {
                 todayTodoList.add(todo.toDo)
             }
         }
 
-        holder.calendarTodoRecyclerView.layoutManager = LinearLayoutManager(holder.calendarTodoRecyclerView.context, RecyclerView.VERTICAL, false)
-        holder.calendarTodoRecyclerView.adapter = CalendarToDoAdapter(todayTodoList)
+        viewHolder.calendarTodoRecyclerView.layoutManager = LinearLayoutManager(viewHolder.calendarTodoRecyclerView.context, RecyclerView.VERTICAL, false)
+        viewHolder.calendarTodoRecyclerView.adapter = CalendarToDoAdapter(todayTodoList)
     }
 
     override fun getItemCount(): Int{
